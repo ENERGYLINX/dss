2814601ba190f6b4e34067615e8da39911dd4061
Devang Mehta <devang@jdmehta.co.uk>
2017-04-17 15:11:19 +0100

    updating build scripts

--- contrib/02-build-maven.sh
+++ contrib/02-build-maven.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-#mvn clean compile package
-#mvn clean compile assemble:jar
-mvn clean install compile assembly:single
+mvn package
--- contrib/recompile.sh
+++ contrib/recompile.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-#mvn clean compile package
-#mvn clean compile assemble:jar
-
 cd /vagrant
 sudo supervisorctl stop java
-sudo mvn clean install compile assembly:single
+sudo mvn package
 sudo supervisorctl start java
